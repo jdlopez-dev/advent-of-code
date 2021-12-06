c55c7f15d238de6b29b63cef02b960e99ffb6faa
Juan <72524993+jdlopez-dev@users.noreply.github.com>
2021-12-06 10:43:15 +0100

    refactor: :recycle: refactor

--- 2021/python_solution/src/advent_of_code_service_test.py
+++ 2021/python_solution/src/advent_of_code_service_test.py
@@ -22,26 +22,25 @@ class AdventOfCodeServiceTest(unittest.TestCase):
                       "00010",
                       "01010"]
 
-    def test_correct_result(self):
+    def test_day_one_part_one(self):
         result = ms.get_number_magnifications_measurements(self.measures)
         self.assertEqual(result, 7)
 
-    def test_correct_result_three_measurement(self):
+    def test_day_one_part_two(self):
         result = ms.get_number_magnifications_three_measurements(self.measures)
         self.assertEqual(result, 5)
 
-    def test_final_position(self):
-
+    def test_day_two_part_one(self):
         final_result = ms.get_final_position(self.positions)
 
         self.assertEqual(final_result, 150)
 
-    def test_final_position_with_aim(self):
+    def test_day_two_part_two(self):
         final_result = ms.get_final_position_with_aim(self.positions)
 
         self.assertEqual(final_result, 900)
 
-    def test_power_consumption(self):
+    def test_day_three_part_one(self):
         power_consumption = ms.get_power_consumption(self.binary_numbers)
         self.assertEqual(power_consumption, 198)
 
